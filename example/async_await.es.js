17d82da8b93b8e1979440e8a141008cce6a31e36
zensh <admin@zensh.com>
2016-09-27 22:26:41 +0800

    add async/await demo run in Node.js v7

---
+++ example/async_await.es.js
@@ -0,0 +1,27 @@
+'use strict'
+// **Github:** https://github.com/toajs/toa
+//
+// **License:** MIT
+
+// `/path-to-node-v7/node --harmony example/async_await.es.js`
+
+const ilog = require('ilog')
+const Toa = require('..')
+
+const app = new Toa()
+
+app.use(function () {
+  this.body = 'support sync function middleware!\n'
+})
+app.use(function (next) {
+  this.body += 'support thunk function middleware!\n'
+  next()
+})
+app.use(function * () {
+  this.body += yield Promise.resolve('support generator function middleware!\n')
+})
+app.use(async function () {
+  this.body += await Promise.resolve('support async/await function middleware!\n')
+})
+
+app.listen(3000, () => ilog.info(`App start at: 3000`))
